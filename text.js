2211ade29ed1fb6613aec322dff0dab041ed4cb7
udonna-cell <orabuezestanley@gmail.com>
2024-10-17 11:34:38 +0100

    move

--- text.js
+++ text.js
@@ -1,6 +1,29 @@
 require("dotenv").config()
-const { API } = require('FaucetPayJS');
-const myAPI = new API(process.env.API_KEY);
+const axios = require('axios');
+const { API } = require('faucetpayjs');
+const myAPI = new API(process.env.FAUCET_KEY);
+// const myAPI = new API(process.env.API_KEY);
 myAPI.getBalance().then(data => {
 	console.dir(data)
 })
+const sendPayment = async () => {
+    const url = 'https://faucetpay.io/api/v1/send';
+    
+    // Replace with your actual values
+    const apiKey = ""; // Your valid API key
+    const amount = "50"; // Amount in satoshis
+    const to = 'stabug6@gmail.com'; // Address or email to send payment to
+    const currency = 'FEY'; // Optional: Default is BTC
+    let referral = false; // Optional: true for referral payment
+    let ipAddress = '192.168.1.1'; // Optional: Your IP address
+
+    try {
+        const response = await myAPI.send(amount, to, currency, referral=false, ipAddress='0.0.0.0')
+
+        console.log('Response:', response.data);
+    } catch (error) {
+        console.error('Error sending payment:', error.response ? error.response.data : error.message);
+    }
+};
+
+sendPayment();
